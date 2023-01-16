8e7dd2aca63528130a25a19be9d8310068613806
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 10:11:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-waterfall--57211",
+  url: "https://restless-waterfall--57211.botics.co",
+  api: "https://restless-waterfall--57211.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
